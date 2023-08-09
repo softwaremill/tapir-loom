55609685415ec73205a3bb607c603099abdedd7c
kciesielski <kciesielski@users.noreply.github.com>
2023-08-09 13:53:11 +0200

    Adjust to changes to Netty configuration API

--- netty/src/main/scala/sttp/tapir/server/netty/loom/NettyIdServerOptions.scala
+++ netty/src/main/scala/sttp/tapir/server/netty/loom/NettyIdServerOptions.scala
@@ -3,47 +3,40 @@ package sttp.tapir.server.netty.loom
 import com.typesafe.scalalogging.Logger
 import sttp.tapir.model.ServerRequest
 import sttp.tapir.server.interceptor.log.{DefaultServerLog, ServerLog}
+import sttp.tapir.server.netty.internal.NettyDefaults
 import sttp.tapir.server.interceptor.{CustomiseInterceptors, Interceptor}
-import sttp.tapir.server.netty.{NettyDefaults, NettyOptions}
 import sttp.tapir.{Defaults, TapirFile}
 
-import java.net.{InetSocketAddress, SocketAddress}
-
-case class NettyIdServerOptions[SA <: SocketAddress](
+case class NettyIdServerOptions(
     interceptors: List[Interceptor[Id]],
     createFile: ServerRequest => TapirFile,
-    deleteFile: TapirFile => Unit,
-    nettyOptions: NettyOptions[SA]
+    deleteFile: TapirFile => Unit
 ) {
-  def prependInterceptor(i: Interceptor[Id]): NettyIdServerOptions[SA] = copy(interceptors = i :: interceptors)
-  def appendInterceptor(i: Interceptor[Id]): NettyIdServerOptions[SA] = copy(interceptors = interceptors :+ i)
-  def nettyOptions[SA2 <: SocketAddress](o: NettyOptions[SA2]): NettyIdServerOptions[SA2] = copy(nettyOptions = o)
+  def prependInterceptor(i: Interceptor[Id]): NettyIdServerOptions = copy(interceptors = i :: interceptors)
+  def appendInterceptor(i: Interceptor[Id]): NettyIdServerOptions = copy(interceptors = interceptors :+ i)
 }
 
 object NettyIdServerOptions {
 
   /** Default options, using TCP sockets (the most common case). This can be later customised using [[NettyIdServerOptions#nettyOptions()]].
     */
-  def default: NettyIdServerOptions[InetSocketAddress] = customiseInterceptors.options
+  def default: NettyIdServerOptions = customiseInterceptors.options
 
-  private def default[SA <: SocketAddress](
-      interceptors: List[Interceptor[Id]],
-      nettyOptions: NettyOptions[SA]
-  ): NettyIdServerOptions[SA] =
+  private def default(
+      interceptors: List[Interceptor[Id]]
+  ): NettyIdServerOptions =
     NettyIdServerOptions(
       interceptors,
       _ => Defaults.createTempFile(),
-      Defaults.deleteFile(),
-      nettyOptions
+      Defaults.deleteFile()
     )
 
   /** Customise the interceptors that are being used when exposing endpoints as a server. By default uses TCP sockets (the most common
     * case), but this can be later customised using [[NettyIdServerOptions#nettyOptions()]].
     */
-  def customiseInterceptors: CustomiseInterceptors[Id, NettyIdServerOptions[InetSocketAddress]] = {
+  def customiseInterceptors: CustomiseInterceptors[Id, NettyIdServerOptions] = {
     CustomiseInterceptors(
-      createOptions =
-        (ci: CustomiseInterceptors[Id, NettyIdServerOptions[InetSocketAddress]]) => default(ci.interceptors, NettyOptions.default)
+      createOptions = (ci: CustomiseInterceptors[Id, NettyIdServerOptions]) => default(ci.interceptors)
     ).serverLog(defaultServerLog)
   }
 
--- netty/src/test/scala/sttp/tapir/server/netty/loom/NettyIdTestServerInterpreter.scala
+++ netty/src/test/scala/sttp/tapir/server/netty/loom/NettyIdTestServerInterpreter.scala
@@ -4,24 +4,21 @@ import cats.data.NonEmptyList
 import cats.effect.{IO, Resource}
 import io.netty.channel.nio.NioEventLoopGroup
 import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.server.netty.NettyOptions
+import sttp.tapir.server.netty.NettyConfig
 import sttp.tapir.server.tests.TestServerInterpreter
 import sttp.tapir.tests.Port
 
-import java.net.InetSocketAddress
-
 class NettyIdTestServerInterpreter(eventLoopGroup: NioEventLoopGroup)
-    extends TestServerInterpreter[Id, Any, NettyIdServerOptions[InetSocketAddress], IdRoute] {
+    extends TestServerInterpreter[Id, Any, NettyIdServerOptions, IdRoute] {
   override def route(es: List[ServerEndpoint[Any, Id]], interceptors: Interceptors): IdRoute = {
-    val serverOptions: NettyIdServerOptions[InetSocketAddress] = interceptors(NettyIdServerOptions.customiseInterceptors).options
+    val serverOptions: NettyIdServerOptions = interceptors(NettyIdServerOptions.customiseInterceptors).options
     NettyIdServerInterpreter(serverOptions).toRoute(es)
   }
 
   override def server(routes: NonEmptyList[IdRoute]): Resource[IO, Port] = {
-    val options = NettyIdServerOptions.default.nettyOptions(
-      NettyOptions.default.eventLoopGroup(eventLoopGroup).randomPort.noShutdownOnClose
-    )
-    val bind = IO.blocking(NettyIdServer(options).addRoutes(routes.toList).start())
+    val config = NettyConfig.defaultNoStreaming.eventLoopGroup(eventLoopGroup).randomPort.withDontShutdownEventLoopGroupOnClose
+    val options = NettyIdServerOptions.default
+    val bind = IO.blocking(NettyIdServer(options, config).addRoutes(routes.toList).start())
 
     Resource
       .make(bind)(binding => IO.blocking(binding.stop()))
